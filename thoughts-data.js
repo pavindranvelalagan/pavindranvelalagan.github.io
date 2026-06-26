823e302217863fc0e5e2fbed70f70a8457106a5c
Pavindran <pavibavi11@gmail.com>
2026-06-27 01:10:07 +0530

    Update

--- thoughts-data.js
+++ thoughts-data.js
@@ -1,27 +1,15 @@
 // Auto-generated THOUGHTS_DATA
 const THOUGHTS_DATA = [
   {
-    "id": "thought-1780244713850",
-    "text": "Here goes another note with another colour. I'm just checking the new feature of my portfolio website, so please come back to this page in a few weeks for a better experience :)",
-    "color": "#fff475",
-    "date": "May 31, 2026"
+    "id": "thought-1782502180954",
+    "text": "Why cant we do the pre processing step of LLMs in local machines, such as extraction of texts from documents, so that we dont rely on their servers and waste our file upload limits? Had this idea for a standalone desktop app where you just drop your PDFs and a generic prompt. The app uses our local CPU to do all the extraction and data cleaning, then gives out a highly detailed, perfectly structured markdown prompt. We just copy and paste that clean text straight into our favourite LLM. We save limits and let the cloud GPUs focus entirely on the actual heavy reasoning!!",
+    "color": "#fdcfe8",
+    "date": "June 27, 2026"
   },
   {
     "id": "introductory-note",
     "text": "Welcome to my Thoughts canvas! This is a dedicated, informal space for tiny thoughts, project ideas, and sometimes act as a tiny diary as well :) Each card is rendered as a colorful sticky note, managed locally by my custom offline CMS.",
     "color": "#cbf0f8",
     "date": "May 31, 2026"
-  },
-  {
-    "id": "why-sticky-notes",
-    "text": "[SAMPLE1] Sticky notes are powerful because they don't demand perfection. They represent active, raw ideas—often under 100 words—that capturing a single insight before it slips away.",
-    "color": "#d7aefb",
-    "date": "May 30, 2026"
-  },
-  {
-    "id": "robotics-hpc-thought",
-    "text": "[SAMPLE2] While deploying SLAM and 3D Gaussian Splatting algorithms, I realized that bla bla bla...",
-    "color": "#ccff90",
-    "date": "May 29, 2026"
   }
 ];
