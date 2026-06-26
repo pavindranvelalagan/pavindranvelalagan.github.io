0d207849f6431a03997bab5da68a9b270479a867
Pavindran <pavibavi11@gmail.com>
2026-06-27 01:13:37 +0530

    Update

--- thoughts-data.js
+++ thoughts-data.js
@@ -2,7 +2,7 @@
 const THOUGHTS_DATA = [
   {
     "id": "thought-1782502180954",
-    "text": "Why cant we do the pre processing step of LLMs in local machines, such as extraction of texts from documents, so that we dont rely on their servers and waste our file upload limits? Had this idea for a standalone desktop app where you just drop your PDFs and a generic prompt. The app uses our local CPU to do all the extraction and data cleaning, then gives out a highly detailed, perfectly structured markdown prompt. We just copy and paste that clean text straight into our favourite LLM. We save limits and let the cloud GPUs focus entirely on the actual heavy reasoning!!",
+    "text": "Why cant we do the pre processing step of LLMs in local machines, such as extraction of texts from documents, so that we dont rely on their servers and waste our file upload limits? Had this idea for a standalone desktop app. It uses our local CPU to do all the extraction and data cleaning, then gives out a highly detailed markdown prompt. We just copy and paste that into our favourite LLM. We save limits and let the cloud GPUs focus entirely on the actual work!!",
     "color": "#fdcfe8",
     "date": "June 27, 2026"
   },
